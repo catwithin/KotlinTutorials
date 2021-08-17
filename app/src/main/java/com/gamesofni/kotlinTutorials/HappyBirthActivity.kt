0d3f76651af477bc9c1d283cef641032578e1f55
catwithin <catwithin@protonmail.com>
2021-08-17 16:27:19 +0200

    unit 1 - pathway 3 - happy birthday card

---
+++ app/src/main/java/com/gamesofni/kotlinTutorials/HappyBirthActivity.kt
@@ -0,0 +1,11 @@
+package com.gamesofni.kotlinTutorials
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HappyBirthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.happy_birth_activity)
+    }
+}
