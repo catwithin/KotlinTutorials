e15e4ff0b8cdee7d173fb38800c39b18c846e9f0
catwithin <catwithin@protonmail.com>
2021-08-18 16:18:07 +0200

    U2P3 - Affirmations - Text RecyclerView

---
+++ app/src/main/java/com/gamesofni/kotlinTutorials/affirmations/AffirmationsActivity.kt
@@ -0,0 +1,25 @@
+package com.gamesofni.kotlinTutorials.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.gamesofni.kotlinTutorials.R
+import com.gamesofni.kotlinTutorials.affirmations.data.Datasource
+
+class AffirmationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_affirmations)
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+
+    }
+}
---
+++ app/src/main/java/com/gamesofni/kotlinTutorials/affirmations/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.gamesofni.kotlinTutorials.affirmations.model
+
+data class Affirmation (val stringResourceId: Int)
